590fcc397533f256d46d9b75475ee3f79d071443
Kristoff Finley <contentconsuming@gmail.com>
2021-01-18 19:25:38 -0700

    Player Rotation
    Changed how rotation works to be more realistic and readable when walking around terrain. The player now faces in the direction they are walking.
    
    Tinkingering with the healthbar, currently bugged.

--- TenebraeProject/Assets/PlayerHealthSlider.cs
+++ TenebraeProject/Assets/PlayerHealthSlider.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerHealthSlider : MonoBehaviour
 {
     float RotateValuez = 0f; //small adjustment removing slight slant
-  
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("arthur_01");
+    }
     // Update is called once per frame
     void LateUpdate()
     {
+        transform.position = player.transform.position;
         transform.LookAt(Camera.main.transform);
+        //transform.rotation = fixedRotation;
         transform.Rotate(0f, 180f, RotateValuez);
+        //change the y position
     }
 }
--- TenebraeProject/Assets/Scripts/PlayerMovement.cs
+++ TenebraeProject/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
 { 
 
     NavMeshAgent agent;
+    Quaternion rotationToLookAt;
 
+    float _speed = 10f;
     public float rotateSpeedMovement = 0.075f;
     float rotateVelocity;
     int LeftMouseButton = 1;
@@ -16,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
     }
 
     // Update is called once per frame
@@ -27,24 +30,47 @@ public class PlayerMovement : MonoBehaviour
             RaycastHit hit;
 
             //Checking if the raycast shot hits something that uses the navmesh system
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
-                //Movement
-                //Have the player move to the raycast point.
+                ////Movement
+                ////Have the player move to the raycast point.
                 agent.SetDestination(hit.point);
 
                 //Rotation
-                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
-                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
-                    rotationToLookAt.eulerAngles.y,
-                    ref rotateVelocity,
-                    rotateSpeedMovement * (Time.deltaTime * 5));
+                //Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                //float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+                //    rotationToLookAt.eulerAngles.y,
+                //    ref rotateVelocity,
+                //    rotateSpeedMovement * (Time.deltaTime * 5));
 
-                transform.eulerAngles = new Vector3(0, rotationY, 0);
+                //transform.eulerAngles = new Vector3(0, rotationY, 0);
+
+                //PROTO-
+                //if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
+                //{
+                //    Vector3 direction = hit.point - transform.position;
+                //    Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+                //    float step = _speed * Time.deltaTime;
+                //    transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, step);
+
+                //    //transform.rotation = Quaternion.RotateTowards();
+                //    //transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);   
+                //}
             }
 
 
         }
-            
+    }
+
+    private void LateUpdate()
+    {
+        if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            //transform.rotation = Quaternion.RotateTowards();
+            transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+        }
+
+
     }
 }
