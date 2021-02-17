7b9e4d6f65910eb9853c79fb4c6cc3629679ce06
Kristoff Finley <contentconsuming@gmail.com>
2021-02-17 12:04:09 -0700

    Enemy Health Bar

---
+++ TenebraeProject/Assets/EnemyUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy;
+
+public class EnemyUI : MonoBehaviour
+{
+    [Range (-10f, 10f)] [SerializeField] float distanceAbove = 1.48f;
+    [Range(-10f, 10f)] [SerializeField] float xPosition = -0.07f;
+    GameObject enemy;
+
+    private void Start()
+    {
+        enemy = transform.parent.gameObject;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 enemyPosition = enemy.transform.position;
+        enemyPosition.y += distanceAbove;
+        //enemyPosition.y = distanceAbove;
+        enemyPosition.x += xPosition;
+        transform.position = enemyPosition;
+        //transform.LookAt(Camera.main.transform);
+        //transform.rotation = fixedRotation;
+        //transform.Rotate(0f, 180f, RotateValuez);
+        //change the y position
+    }
+}
