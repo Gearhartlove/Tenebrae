fa9ed9b40549432633abb0ae899bed65a281be84
Kristoff Finley <contentconsuming@gmail.com>
2021-03-06 14:48:50 -0700

    Ranged Attack Work

--- TenebraeProject/Assets/Scripts/Player/PlayerCombat.cs
+++ TenebraeProject/Assets/Scripts/Player/PlayerCombat.cs
@@ -7,54 +7,48 @@ using Enemy;
 
 public class PlayerCombat : MonoBehaviour
 {
-    private float RotationSpeed = 500f;
     public float Distance = -1f;
     public float AttackRange = 10f;
     public bool Target = false;
     public bool InRange = false;
-    private bool AttentionReset = true;
-    GameObject TargetedEnemy;
+    private bool InCombat = false;
+    public bool Attacking = true;
+    public GameObject TargetedEnemy;
+
+    //projectile variables
+    public GameObject projectile;
+    public Transform shotPoint;
+    public float AttackCooldown = 0; //rename later 
+    public float AttackInterval = 3; //rename later
 
     private void Update()
     {
         //(directly below) lets player move the enemy after it has been selected, so that they 
         //can move elswhere
-        if (Target)
+        if (InCombat)
         {
             DetermineDistance(TargetedEnemy.transform.position); //Is the player in range?
+            if (PlayerVariables.PlayerFocus == "Enemy")
+            {
+                if (InRange) { PerformAttack(); }
+                else if (!InRange) { MovePlayer(); }
+            }
+            else { Attacking = false; }
 
-            if (Player.PlayerVariables.PlayerFocus == "Enemy") //only care about attacking / moving when focus is the enemy
-            {
-                if (InRange) { PerformAttack(TargetedEnemy); } //determine distance and attack
-                else { MovePlayer(); }  //move player
-            }
-        }
-    }
-
-    //Rotating the player when in range of the target (close combat) 
-    private void LateUpdate()
-    {
-        if (Target)
-        {
-            if (InRange)
-            {
-                DeterminePlayerRotation(TargetedEnemy);
-                AttentionReset = false;
-            }
-            else if (AttentionReset == false)
-            {
-                ResetAttention(); //rotates the player normally
-                AttentionReset = true;
-            }
+            if (Target) { AttackCooldown -= Time.deltaTime; }
         }
     }
 
     //Set targeted enemy, determine the distance between player and enemy
     public void Attack(GameObject Enemy)
     {
-        TargetedEnemy = Enemy;
-        Target = true;
-        DetermineDistance(Enemy.transform.position);
+        if (!Target)
+        {
+            InCombat = true;
+            TargetedEnemy = Enemy;
+            Target = true;
+            //DetermineDistance(Enemy.transform.position);
+        }
     }
 
     //Determine distance between player and enemy
@@ -74,44 +68,29 @@ public class PlayerCombat : MonoBehaviour
     }
 
     //Perform the attack 
-    private void PerformAttack(GameObject Enemy)
+    private void PerformAttack()
     {
-        //Stop player from moving
-        if (Player.PlayerVariables.PlayerFocus == "Enemy")
+        
+        //Stop player from moving 
+        PlayerVariables.Agent.SetDestination(PlayerVariables.PlayerGameObject.transform.position);
+
+        //rotate towards target
+        //Attack
+        if (AttackCooldown <= 0)
         {
-            //Stop player from moving 
-            Player.PlayerVariables.Agent.SetDestination(Player.PlayerVariables.PlayerGameObject.transform.position);
-            //Attack
-            Debug.Log("Whacked!!!");
+            //fire projectile
+            RotateTowards(TargetedEnemy.transform.position);
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+            AttackCooldown = AttackInterval;
         }
     }
 
-    private void DeterminePlayerRotation(GameObject Enemy)
+    private void RotateTowards(Vector3 to)
     {
-        ////Rotating agent around objects smoothly when agent is moving
-        //if (Player.PlayerVariables.Agent.velocity.sqrMagnitude > Mathf.Epsilon)
-        //{
-        //    //Step size is equal to speed times frame time (needed for RotateTowarwds paramter below)
-        //    var step = RotationSpeed * Time.deltaTime;
-        //    if (InRange)
-        //    {
-        //        Vector3 vector = Enemy.transform.position - Player.PlayerVariables.PlayerGameObject.transform.position;
-        //        Quaternion LookAtRotation = Quaternion.LookRotation(vector.normalized);
-        //        transform.rotation = Quaternion.Lerp(transform.rotation, LookAtRotation, step); //smooth rotation between old and new rotation}
-        //    }          
-        //}
-        
-    }
-    
-    private void ResetAttention()
-    {
-        
-        //transform.rotation.Set(transform.rotation.x, transform.rotation.y, transform.rotation.z,1);
-        //Step size is equal to speed times frame time (needed for RotateTowarwds paramter below)
-        //var step = 24f * Time.deltaTime;
-        //Quaternion oldRotation = transform.rotation; //current rotation of the agent
-        //Quaternion newRotation = Quaternion.LookRotation(Player.PlayerVariables.Agent.velocity.normalized); //direction the agent is moving
-        //Player.PlayerVariables.PlayerGameObject.transform.rotation = Quaternion.Lerp(oldRotation, newRotation, step); //smooth rotation between old and new rotation}
+        Quaternion _lookRotation =
+    Quaternion.LookRotation((to - transform.position).normalized);
+
+        transform.rotation = _lookRotation;
     }
 }
   
---
+++ TenebraeProject/Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    private PlayerCombat playerCombat;
+    public GameObject Target;
+
+    //public GameObject destroyEffect;
+    private void Awake()
+    {
+        playerCombat = PlayerVariables.PlayerGameObject.GetComponentInChildren<PlayerCombat>();
+    }
+
+    private void Start()
+    {
+        //Invoke("DestroyProjectile", lifetime);
+    }
+
+    private void Update()
+    {
+        Target = playerCombat.TargetedEnemy;
+        Vector3 TargetDestination = Target.transform.position;
+        transform.Translate(-TargetDestination * speed * Time.deltaTime);
+    }
+
+    private void DestroyProjectile()
+    {
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void OnColliderEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        DestroyProjectile();
+    }
+}
